d18df147664521e896174610e371c787edb887eb
ViduminiArdarsha <ghvidumini@gmail.com>
2025-07-11 01:04:40 +0530

    navbar building

---
+++ app/Navbar.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+import { BiBug } from "react-icons/bi";
+
+const Navbar = () => {
+
+  const links=[
+    {label:'Dashboard',href:'/'},
+    {label:'Issues',href:'/issues'}
+  ]
+
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-10 h-15 items-center'>
+        <Link href="/"><BiBug /></Link>
+
+        <ul className='flex space-x-6'>
+            {links.map(link=>
+            <Link 
+              key={link.href}
+              href={link.href} 
+              className=' text-zinc-500 hover:text-zinc-800 transition-colors'>
+              {link.label}</Link>)}
+        </ul>   
+    </nav>
+  )
+}
+
+export default Navbar

99f1593769a46d26d0f9eabcad20a9b25ef80a2e
ViduminiArdarsha <ghvidumini@gmail.com>
2025-07-11 01:25:40 +0530

    active link on navbar fixed

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,8 +1,13 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BiBug } from "react-icons/bi";
 
 const Navbar = () => {
+
+  const currentPath = usePathname();
+  
 
   const links=[
     {label:'Dashboard',href:'/'},
@@ -17,8 +22,8 @@ const Navbar = () => {
             {links.map(link=>
             <Link 
               key={link.href}
-              href={link.href} 
-              className=' text-zinc-500 hover:text-zinc-800 transition-colors'>
+              className={`${link.href===currentPath?'text-zinc-800':'text-zinc-500'} hover:text-zinc-800 transition-colors`}
+              href={link.href}>
               {link.label}</Link>)}
         </ul>   
     </nav>

92996c95ee47b1ed3b5f03b801623df059749846
ViduminiArdarsha <ghvidumini@gmail.com>
2025-07-17 18:11:21 +0530

    Displaying issues from DB

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import { Button } from '@radix-ui/themes';
-import Link from 'next/link';
+import React from "react";
+import { Button, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import { prisma } from "@/prisma/client";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
     <div>
-      <Button><Link href = "/issues/new">New Issue</Link></Button>
-    </div>
-  )
-}
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
 
-export default IssuesPage
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Created At</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>{issue.id}</Table.Cell>
+              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell>{issue.status}</Table.Cell>
+              <Table.Cell>{issue.createdAt.toDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
+  );
+};
+
+export default IssuesPage;
